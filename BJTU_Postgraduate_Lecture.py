31bc09867efa6b4a999e1f9841bb196a399ed829
CHAOSPOLARIS <57836035+CHAOSPOLARIS@users.noreply.github.com>
2019-11-17 16:51:45 +0800

    Add files via upload

---
+++ BJTU_Postgraduate_Lecture.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("http://hiray666.mikecrm.com/kDpGeKw")
+while 1==1:
+    try:
+        driver.find_element(By.CSS_SELECTOR, ".fbi_input").send_keys("Your name")
+        driver.find_element(By.CSS_SELECTOR, ".wrapper").click()
+        driver.find_element(By.CSS_SELECTOR, ".is_focus > .fbi_input").send_keys("Your student ID")
+        driver.find_element(By.ID, "form_submit").click()
+        if driver.find_element(By.ID, "form_submit"):
+            print('success')
+            break
+    except:
+        print('wait')
